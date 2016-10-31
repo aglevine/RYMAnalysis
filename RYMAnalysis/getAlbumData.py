eefcbc084025a804b8e433d7ba389515b446cbcd
aglevine <aglevine@wisc.edu>
2016-10-31 06:13:29 +0100

    Added file descriptions

--- RYMAnalysis/getAlbumData.py
+++ RYMAnalysis/getAlbumData.py
@@ -1,4 +1,5 @@
 # Script to parse html file from RYM
+#creates a csv file for R
 
 from sys import argv
 import math
@@ -36,8 +37,7 @@ for line in f.readlines():
            name = name.replace("_", " ")
            name = name.title()
            if (  float(rating) >= minRate and  int(votes) >= minVote):
-           	#print "Rating: " + rating + " Votes: " + votes + " Name: " + name + " Genre: " + genre
-		if (shift==True):
+		if (shift==True): #work in progress, testing out corrections for year
 			ratingFloat = float(rating)
 			yearFloat = float(year)
                         if (yearFloat >= 2008):
@@ -45,16 +45,5 @@ for line in f.readlines():
  				mean = 3.532398-0.01474833333*(yearFloat-2008.0)
 				ratingFloat = ratingFloat*0.16/sd
 				ratingFloat = ratingFloat - mean*0.16/sd+3.66
-				#ratingFloat = ratingFloat - mean + 3.66
-				#if (ratingFloat < 3.66):
-			#		ratingFloat= ratingFloat-(3.66-ratingFloat)*.5
-				#ratingFloat = (ratingFloat - mean)*(ratingFloat-mean)
-				#ratingFloat = ratingFloat/(2*0.12*0.12)
-				#if ((ratingFloat - math.log(0.16/0.12)) < 0):
-			#		ratingFloat =abs(ratingFloat - math.log(0.16/0.12))
-		#			ratingFloat = -1*math.sqrt(ratingFloat)*math.sqrt(2)*0.16+3.66
-                #                else:
-#					ratingFloat =abs(ratingFloat - math.log(0.16/0.12))
-#                                        ratingFloat = math.sqrt(ratingFloat)*math.sqrt(2)*0.16+3.66
 				rating = str(ratingFloat)
                 print rating+","+votes+","+year+","+period+","+name
