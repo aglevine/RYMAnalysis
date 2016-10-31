19383a2f2dce93136c3ed86210a4139fbf639ec8
aglevine <aglevine@wisc.edu>
2016-10-31 05:19:34 +0100

    Added mean plots for years

--- RYMAnalysis/getAlbumData.py
+++ RYMAnalysis/getAlbumData.py
@@ -1,14 +1,25 @@
 # Script to parse html file from RYM
 
 from sys import argv
+import math
 
+sdMap= {"2008": 0.11, "2009": 0.11, "2010":0.12, "2011":0.12,"2012":0.11,
+"2013":0.12,"2014":0.12} 
 
+shift = True
 fileName=argv[1]
 minRate = float(argv[2])
 minVote = float(argv[3])
 f=open(fileName,'r')
-genre =""
-print "RATING,VOTES"
+period =""
+year = fileName[fileName.find("top")+2+1:fileName.find("RYM")]
+intYear = int(year)
+if (intYear <= 2000):
+	period = "PreRYM"
+elif (intYear <=2007):
+	period = "NewRYM"
+elif (intYear >= 2008):
+	period = "PresentRYM"
 for line in f.readlines():
         if ('href="/genre/' in line):
            genre = line[line.find('href="/genre/')+12+1:line.find('/">')]
@@ -26,4 +37,24 @@ for line in f.readlines():
            name = name.title()
            if (  float(rating) >= minRate and  int(votes) >= minVote):
            	#print "Rating: " + rating + " Votes: " + votes + " Name: " + name + " Genre: " + genre
-                print rating+","+votes
+		if (shift==True):
+			ratingFloat = float(rating)
+			yearFloat = float(year)
+                        if (yearFloat >= 2008):
+				sd = sdMap[year]
+ 				mean = 3.532398-0.01474833333*(yearFloat-2008.0)
+				ratingFloat = ratingFloat*0.16/sd
+				ratingFloat = ratingFloat - mean*0.16/sd+3.66
+				#ratingFloat = ratingFloat - mean + 3.66
+				#if (ratingFloat < 3.66):
+			#		ratingFloat= ratingFloat-(3.66-ratingFloat)*.5
+				#ratingFloat = (ratingFloat - mean)*(ratingFloat-mean)
+				#ratingFloat = ratingFloat/(2*0.12*0.12)
+				#if ((ratingFloat - math.log(0.16/0.12)) < 0):
+			#		ratingFloat =abs(ratingFloat - math.log(0.16/0.12))
+		#			ratingFloat = -1*math.sqrt(ratingFloat)*math.sqrt(2)*0.16+3.66
+                #                else:
+#					ratingFloat =abs(ratingFloat - math.log(0.16/0.12))
+#                                        ratingFloat = math.sqrt(ratingFloat)*math.sqrt(2)*0.16+3.66
+				rating = str(ratingFloat)
+                print rating+","+votes+","+year+","+period+","+name
